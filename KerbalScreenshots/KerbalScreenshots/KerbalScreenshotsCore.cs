3cd0fa42cabd10e833bec847a0c50321c1f3964d
Ainacteru <gbenawijr@gmail.com>
2025-05-26 18:22:05 -0700

    Kirbel test

--- KerbalScreenshots/KerbalScreenshots/KerbalScreenshotsCore.cs
+++ KerbalScreenshots/KerbalScreenshots/KerbalScreenshotsCore.cs
@@ -1,7 +1,13 @@
-﻿namespace KerbalScreenshots
-{
-    public class KerbalScreenshotsCore
-    {
+﻿using UnityEngine;
 
+namespace KerbalScreenshots
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+
+    public class KerbalScreenshotsCore : MonoBehaviour {
+
+        private void Start() {
+            Debug.Log("KERBAL SCREENSHOTS WORKS!!! yay")
+        }
     }
 }
